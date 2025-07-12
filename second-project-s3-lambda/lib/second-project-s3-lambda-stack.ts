c32d89ebda4ca50e35fbbef1d4602bdbf7f559d1
Souraevshing <isauravshing@gmail.com>
2025-07-12 13:08:27 +0530

    ✨ (route): add route
    feat: add GET / home route

--- second-project-s3-lambda/lib/second-project-s3-lambda-stack.ts
+++ second-project-s3-lambda/lib/second-project-s3-lambda-stack.ts
@@ -5,19 +5,50 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from "path";
 
+import * as apiGateway from "aws-cdk-lib/aws-apigatewayv2";
+import * as apiGatewayIntegrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+
 export class SecondProjectS3LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaExample = new NodejsFunction(this, "lambda", {
+    // simple example showcasing lambda createConnection, logging, testing and deploying
+    const exampleLambda = new NodejsFunction(this, "exampleLambda", {
       runtime: lambda.Runtime.NODEJS_22_X,
       entry: path.join(__dirname, "../src/lambda/handler.ts"),
       handler: "lambdaExample1",
-      functionName: "SecondProjectS3LambdaStack",
+      functionName: `${this.stackName}-ExampleLambdaStack`,
     });
+
+    // send output to aws console
     new cdk.CfnOutput(this, "lambdaExample1Output", {
-      value: lambdaExample.functionArn,
+      value: exampleLambda.functionArn,
       description: "ARN of lambdaExample1",
+    });
+
+    const homeLambda = new NodejsFunction(this, "homeLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      entry: path.join(__dirname, "../src/lambda/handler.ts"),
+      handler: "home",
+      functionName: `${this.stackName}-HomeRouteLambdaStack`,
+    });
+
+    const http = new apiGateway.HttpApi(this, "firstHttpApi", {
+      apiName: "First Http API",
+      description: "First Http API with CDK",
+    });
+    http.addRoutes({
+      path: "/",
+      methods: [apiGateway.HttpMethod.GET],
+      integration: new apiGatewayIntegrations.HttpLambdaIntegration(
+        "HomeRouteIntegration",
+        homeLambda
+      ),
+    });
+
+    new cdk.CfnOutput(this, "HttpUrl", {
+      value: http.url!,
+      description: "Http API Url",
     });
   }
 }
--- second-project-s3-lambda/src/lambda/handler.ts
+++ second-project-s3-lambda/src/lambda/handler.ts
@@ -1,7 +1,21 @@
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+
 export const lambdaExample1 = async (e: any) => {
   console.log("Hi from function lambda");
   return {
     status: 200,
     message: "Hello from function lambda",
+  };
+};
+
+//home route
+export const home = async (e: APIGatewayProxyEventV2) => {
+  console.log("Hello from home route", e);
+
+  return {
+    status: 200,
+    body: JSON.stringify({
+      message: "Hello from home route",
+    }),
   };
 };
